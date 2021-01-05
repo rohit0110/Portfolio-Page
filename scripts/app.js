da60da7285e4c4e028f8f9c0f3b854f790551e56
rohit0110 <rohit7rathore15@gmail.com>
2021-01-05 23:49:13 +0530

    Chose a dark color scheme for everything/Removed bug of dropdown autoscrolling

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,9 @@ const navSlide = () =>{
     console.log("words");
     const toggleButton=document.querySelector('.toggle-button');
     const nav=document.querySelector('.nav-links');
-    toggleButton.addEventListener('click',()=>{
+    toggleButton.addEventListener('click',(event)=>{
         nav.classList.toggle('active');
+        event.preventDefault()
     });
 }
 
